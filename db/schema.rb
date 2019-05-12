d5be7fc45ff4e37f2432816c9256f548bc420dd8
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-05-12 15:52:33 +0100

    Generate ClassSession model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_140624) do
+ActiveRecord::Schema.define(version: 2019_05_12_144910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "class_sessions", force: :cascade do |t|
+    t.string "discipline"
+    t.string "day"
+    t.time "start_time"
+    t.time "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "disciplines", force: :cascade do |t|
     t.string "name"
