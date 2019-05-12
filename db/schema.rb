5e0b0e04a9e640d088b54d6b06a866693cf07c50
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-05-12 15:58:21 +0100

    Generate Trainer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_144910) do
+ActiveRecord::Schema.define(version: 2019_05_12_145407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 2019_05_12_144910) do
     t.string "title"
     t.string "photo_url"
     t.string "link_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.string "description"
+    t.string "photo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
