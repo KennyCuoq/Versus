42a71a6999bc86d0c4c72ebb40cde67b42597caf
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-05-12 14:58:04 +0100

    Generate Discipline Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_12_135225) do
+ActiveRecord::Schema.define(version: 2019_05_12_135402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "disciplines", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "home_banners", force: :cascade do |t|
     t.string "photo_url"
