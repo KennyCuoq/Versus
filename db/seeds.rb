cc57bf6fccd9e9b7ad34911406cc68553cbd344e
victor king <victor.king@essec.edu>
2019-06-08 17:19:33 +0800

    bootstrapped the navbar for mobile

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,20 +18,20 @@ Trainer.destroy_all
 
 # Set up attributes for disciplines
 muay_thai = {
-  name: 'MUAY THAI',
+  name: 'Muay Thai',
   remote_photo_url: 'http://www.muaythaischolar.com/wp-content/uploads/2016/12/Kickers.jpg',
   description: "Also known as the 'Science of eight limbs', Muaythai is the ancient martial art and national sport of Thailand. Muaythai combines punches, kicks, knee and elbow strikes with clinching and sweeps. It’s considered to be one of the most effective of the striking arts. Our Muaythai classes are authentic from experienced trainers/fighters, in addition to a great workout, they will teach you proper techniques regardless of whether you decide to fight or not."
 }
 
 mma = {
-  name: 'MMA',
+  name: 'Mixed Martial Arts',
   remote_photo_url: 'https://www.tigermuaythai.com/wp-core/wp-content/uploads/2008/10/cody-no-love-ufc-tmt.jpg',
   description: "MMA is a full contact combat sport that allows the use of both striking and grappling techniques, both standing and on the ground. It could include a range of martial arts including boxing, wrestling, Brazilian jiu-jitsu, Muaythai, kickboxing, taekwondo, karate, judo and other styles.
   Aside from going into competitions, MMA is a great overall workout regime suitable for both men and women. Children and teens can benefit from the disciplinary nature of the training – increase focus, concentration as well as character-building."
 }
 
 bjj = {
-  name: 'BJJ',
+  name: 'Braziilian Jiu-Jitsu',
   remote_photo_url: 'https://i.pinimg.com/originals/2d/02/cc/2d02cc5f5a72fcde54124387a8362ef7.jpg',
   description: "Known as the ‘gentle art’, Brazilian jiujitsu (BJJ) is a martial art, combat sport, and a self defense system that focuses on grappling and especially ground fighting. The art was derived from the japanese martial art of kodokan judo (which itself is derived from japanese jujutsu) in the early 20th century. It teaches that a smaller, weaker person can successfully defend against a bigger, stronger assailant by using leverage and proper technique – most notably by applying joint-locks and chokeholds to defeat the other person. BJJ training can be used for sport grappling tournaments (gi and no-gi) and is essential to mixed martial arts (MMA) training or self defense. Sparring (commonly referred to as “rolling”) and live drilling play a major role in training, and a premium is placed on performance, especially in competition, in relation to progress and ascension through its ranking system."
 }
