fd290e056c8816ceb1e1da8e97bfaba5da905536
victor king <victor.king@essec.edu>
2019-06-23 16:45:33 +0800

    added devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   validates :email, presence: true
 end
