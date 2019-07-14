2d5e7e2c96bcd31fc9f5c0a3b73ff7e4c909066c
Kenny Cuoq <kenny.cuoq@gmail.com>
2019-07-14 16:40:39 +0100

    Fix admin console permission

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :registerable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
-  validates :email, presence: true
+  validates :email, :password, presence: true
 end
 
 # 852Train-like_a/Fighter!
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,17 +1,17 @@
 RailsAdmin.config do |config|
 
   # Admin
-  config.authorize_with do
-   redirect_to root_path unless warden.user.admin == true
-  end
+  # config.authorize_with do
+  #  redirect_to root_path unless !warden.user.nil?
+  # end
 
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
